27d68d1ea9eeae740e987d8bfe5f4a67f36079cf
MADHU6239 <m89068118@gmail.com>
2025-12-12 19:01:17 +0530

    Python question.py

---
+++ Python question.py
@@ -0,0 +1,11 @@
+What is python?
+What is variable?
+How do you take input in python?
+What is the difference between a string,integer and float?
+What does the print() function do?
+Can you add two number taken from user?
+What is type casting?
+why can't we use class as a variable name?
+what is keyword?
+How many type of keywords?
+How do you show multiple items in print?
