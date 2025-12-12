55a6e9a7f4b93dcd2bbc3ac0af9ca48c16ffbf7f
MADHU6239 <m89068118@gmail.com>
2025-12-12 11:22:48 +0530

    Create if-else & logic.py

---
+++ if-else & logic.py
@@ -0,0 +1,33 @@
+# Day 2 - Input, If-Else, Logic
+
+# Taking input
+name = input("Enter your name: ")
+print("Hello", name)
+
+# Two number sum
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Sum =", a + b)
+
+# Temperature conversion
+c = float(input("Enter temperature in Celsius: "))
+f = (c * 9/5) + 32
+print("Temperature in Fahrenheit:", f)
+
+# Pass or Fail
+marks = int(input("Enter marks: "))
+if marks >= 33:
+    print("Pass")
+else:
+    print("Fail")
+
+# Nested If example
+age = int(input("Enter your age: "))
+if age >= 18:
+    gender = input("Enter gender (M/F): ")
+    if gender == "M":
+        print("You are an Adult Male")
+    else:
+        print("You are an Adult Female")
+else:
+    print("You are a Minor")
